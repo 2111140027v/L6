1b62fd261c86fe74593770ad458df350d2369eb0
Haruto Nakaji <2111140027v@ed.fuk.kindai.ac.jp>
2023-11-08 05:09:53 +0000

    show.html.erb編集

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,6 @@
 class CartsController < ApplicationController
   def show
     @cart = current_cart
+    @cartitems = CartItem.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   #root 'products#index'
   root 'top#main'
   get 'top/main'
+  post 'product/new'
+  post 'cart_items/new'
+  post 'cart_items/create'
 
   resources :cart_items, only: [:new, :create, :destroy]
   resources :carts, only: [:show]
